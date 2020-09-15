e4ed020012cf7f901f2967cb927ed490d42bd14b
lgy <1004216801@qq.com>
2020-09-15 18:16:20 +0800

    sss

--- src/main/java/com/persagy/quartz/job/WorkorderSimpleJob.java
+++ src/main/java/com/persagy/quartz/job/WorkorderSimpleJob.java
@@ -9,11 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-
+666
 import java.util.Random;
 www
 
-存储
+存储66
 得得@PersistJobDataAfterExecution
 public class WorkorderSimpleJob extends QuartzJobBean {
 	@Autowired
