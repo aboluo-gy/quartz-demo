444af9015f3b1c7f2aea8f1d8b270c9ab15b83be
lgy <1004216801@qq.com>
2020-09-17 22:22:54 +0800

    废弃之前提交

--- src/main/java/com/persagy/quartz/job/MyJob.java
+++ src/main/java/com/persagy/quartz/job/MyJob.java
@@ -15,9 +15,7 @@ import java.util.Random;
 @DisallowConcurrentExecution
 @PersistJobDataAfterExecution
 public class MyJob extends QuartzJobBean {
-顶顶顶
-顶顶顶
-1111
+
     @Autowired
     Hello hello;
     private static final Logger LOGGER = LoggerFactory.getLogger(MyJob.class);
